76d3a28402aabd6d236723d64641372b2b483031
Arpan888-lang <choudharyarpan8368@gmail.com>
2024-11-11 20:29:53 +0000

    Datatypes Summary

---
+++ basics1/datatypeSummary.js
@@ -0,0 +1,25 @@
+const myFunct = function(){
+    console.log("my function");
+    
+}
+
+myFunct();
+console.log(typeof myFunct);
+
+let obj = {
+    name : "arpan",
+    upiID : "chauarpa@kotak",
+}
+
+console.log(obj);
+console.log(typeof obj);
+console.log(obj.name);
+
+let outTemp = null;
+console.log(typeof null);
+
+let state;
+console.log(typeof state);
+
+
+
