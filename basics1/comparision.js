8c379f91cea3cf6017e2ee82a3b6c5ca3925f7ef
Arpan888-lang <choudharyarpan8368@gmail.com>
2024-11-11 20:14:16 +0000

    comparison operators

---
+++ basics1/comparision.js
@@ -0,0 +1,14 @@
+console.log(null < 0);
+console.log(null == 0);
+
+console.log(null>=0);
+
+console.log("2" > 1);
+// js will automatically convert the string into number.
+
+// === it checks the and the datatype
+
+console.log("4"=== 4);
+
+
+
